12b5f44e6868fb798b2794a5d766a553906f5e52
Arthurdw <mail@arthurdw.com>
2021-06-17 17:31:40 +0200

    :bug: Forgot to import these here.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-import { Icon as ico } from './components/Image';
+import {
+  ApplicationsIcon as appsIco,
+  BellIcon as bellIco,
+  CoinsIcon as coinsIco,
+  DashboardIcon as dashIco,
+  DocumentationIcon as docIco,
+  Icon as ico,
+  InboxIcon as inboxIco,
+  SearchIcon as searchIco,
+  SettingsIcon as settingsIco,
+} from './components/Image';
 
 export const Icon = ico;
+export const DashboardIcon = dashIco;
+export const DocumentationIcon = docIco;
+export const ApplicationsIcon = appsIco;
+export const InboxIcon = inboxIco;
+export const BellIcon = bellIco;
+export const SettingsIcon = settingsIco;
+export const SearchIcon = searchIco;
+export const CoinsIcon = coinsIco;
