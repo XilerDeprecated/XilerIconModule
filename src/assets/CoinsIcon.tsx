14b500227ade750c273b3d4c119d3bfe20d73899
Arthurdw <mail@arthurdw.com>
2021-07-14 12:45:43 +0200

    :bug: React error removal for prop names.

--- src/assets/CoinsIcon.tsx
+++ src/assets/CoinsIcon.tsx
@@ -11,21 +11,21 @@ export const CoinsIcon: React.FC<SVGProps> = (props) => (
     <path
       d="M8.22501 16.3C4.23476 16.3 1.00001 12.875 1.00001 8.65C1.00001 4.42502 4.23476 1 8.22501 1C12.2153 1 15.45 4.42502 15.45 8.65C15.45 12.875 12.2153 16.3 8.22501 16.3Z"
       stroke={props.color ?? COLOR}
-      stroke-width={STROKE_WIDTH}
-      stroke-linecap={LINE}
-      stroke-linejoin={LINE}
+      strokeWidth={STROKE_WIDTH}
+      strokeLinecap={LINE}
+      strokeLinejoin={LINE}
     />
     <path
       d="M10.35 5.93C9.80901 5.49131 9.134 5.25131 8.4375 5.25C6.67715 5.25 5.25 6.7715 5.25 8.65C5.25 10.5285 6.67715 12.05 8.4375 12.05C9.1549 12.05 9.81705 11.7967 10.35 11.37"
       stroke={props.color ?? COLOR}
-      stroke-width={STROKE_WIDTH}
-      stroke-linecap={LINE}
-      stroke-linejoin={LINE}
+      strokeWidth={STROKE_WIDTH}
+      strokeLinecap={LINE}
+      strokeLinejoin={LINE}
     />
     <path
       d="M8.64999 1C11.7669 1 18 1.765 18 8.65C18 15.535 11.7669 16.3 8.64999 16.3"
       stroke={props.color ?? COLOR}
-      stroke-width={STROKE_WIDTH}
+      strokeWidth={STROKE_WIDTH}
     />
   </svg>
 );
